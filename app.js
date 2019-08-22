4285e8dda6ff8b5ccc681edcb5e6968e1d198d5a
jvikre <julievikre@gmail.com>
2019-08-21 22:16:31 -0400

    update to bootcamp 4

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ var commentRoutes       = require("./routes/comments"),
       
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -60,6 +61,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
    console.log("YelpCamp server has started...") 
 });
